bdbeff6a96de8c8dcf0f1014a55b216bc55a02ad
Bonbox562 <Boon56228@gmail.com>
2024-05-20 14:21:05 +0900

    Update weather_color.glsl

--- shaders/include/light/colors/weather_color.glsl
+++ shaders/include/light/colors/weather_color.glsl
@@ -6,15 +6,15 @@
 uniform float biome_may_sandstorm;
 
 vec3 get_rain_color() {
-	return mix(0.075, 0.66, smoothstep(-0.1, 0.5, sun_dir.y)) * sunlight_color * vec3(0.35, 0.55, 1.00);
+	return mix(0.1, 0.85, smoothstep(-0.1, 0.5, sun_dir.y)) * sunlight_color * vec3(0.3, 0.45, 1.00);
 }
 
 vec3 get_snow_color() {
-	return mix(0.075, 1.60, smoothstep(-0.1, 0.5, sun_dir.y)) * sunlight_color * vec3(0.35, 0.55, 1.00);
+	return mix(0.1, 0.85, smoothstep(-0.1, 0.5, sun_dir.y)) * sunlight_color * vec3(0.35, 0.55, 1.00);
 }
 
 vec3 get_sandstorm_color() {
-	return mix(0.075, 0.66, smoothstep(-0.1, 0.5, sun_dir.y)) * sunlight_color * vec3(1.00, 0.80, 0.60);
+	return mix(0.1, 0.85, smoothstep(-0.1, 0.5, sun_dir.y)) * sunlight_color * vec3(1.00, 0.80, 0.60);
 }
 
 vec3 get_weather_color() {
